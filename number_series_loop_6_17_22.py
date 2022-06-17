af7a2d8379c0ca0ec6ca7caa5c21630d88f998c3
Ron S. Medrano <83150978+jbdcruz123@users.noreply.github.com>
2022-06-17 21:12:01 +0800

    number_series_loop_6_17_22.py
    number_series_loop_6_17_22.py

---
+++ number_series_loop_6_17_22.py
@@ -0,0 +1,19 @@
+
+#number series of loop
+
+#statment 1
+n = 4
+i =0
+mx = 0
+ad = 1
+while i < n+1:
+      mx = ad
+      ad +=2
+      i+=1
+print(f"i {i}, n {n}, mx {mx}, ad {ad} \n")
+
+#another way using aritmetic one statment only
+#same as statment one
+mx = (n *2) +1
+
+print(f"another way: mx {mx}, n { n}")
