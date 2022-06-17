089eceb3acc4d9a89ba6304c7b2b5cdb6e40089b
Ron S. Medrano <83150978+jbdcruz123@users.noreply.github.com>
2022-06-17 21:04:16 +0800

    pyramid_6_17_22.py
    pyramid_6_17_22.py

---
+++ pyramid_6_17_22.py
@@ -0,0 +1,30 @@
+#RSM pyramid 6/17/2022
+def fPyramid(n_p):
+      n = n_p
+      mx = ( n* 2) +1
+      i =0
+      ad =1
+      n_l = n+ 2
+      md = int(mx /2) 
+      while i < n_l:     
+            j =0
+            while j < mx:
+                  if i == 0 or i == n_l-1:
+                        #controls the top margin and bottom margin
+                        print("a", end="")
+                  else: 
+                        #i = 1 to n_l -1 (the body part of the pyramid)                 
+                        if j >= md - int(ad /2) and j<= md + int(ad /2):
+                              print(" ", end="")
+                        else:
+                              print("a", end="")                                
+                  j+=1
+            print()
+            if i >= 1 :
+                  ad+=2
+            i+=1
+print("input pyramid: ", end="")
+fPyramid(int(input()))
+
+
+
